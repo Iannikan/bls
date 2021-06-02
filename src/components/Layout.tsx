e673f374e94c17ff5cc2cd043fc7ef68a8910d2e
ianniklason <imniklason@gmail.com>
2021-06-01 20:25:37 -0600

    added global styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import React, { useState, useEffect, Fragment } from 'react';
-import { css, jsx, useTheme } from '@emotion/react';
+import { css, jsx, useTheme, Global } from '@emotion/react';
 import { Helmet } from 'react-helmet';
 import Navbar from './Navbar';
 
@@ -12,14 +12,19 @@ const Layout: React.FC = ({ children }) => {
   const styledContainer = css({
     width: '100vw',
     height: '100vw',
-    boxSizing: 'border-box',
   })
   const styledMain = css({
     display: 'flex',
     justifyContent: 'center',
   })
+  const GlobalStyles = () => <Global styles={css({
+    '*': {
+      boxSizing: 'border-box',
+    }
+  })} />
   return (
     <div css={styledContainer}>
+      {GlobalStyles()}
       <Helmet title={"Labor Statistics Map"} />
       <Navbar />
       <main css={styledMain}>
